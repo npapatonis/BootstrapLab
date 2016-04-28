784b1bc51c5313695ea6d71069438f80e7eea180
Nick Papatonis <npapatonis@guard1.com>
2016-04-28 18:47:21 -0400

    Date range validation is now performed only on fromDate input.

--- BootstrapLab/App/Views/validationLabFour/validationLabFour.js
+++ BootstrapLab/App/Views/validationLabFour/validationLabFour.js
@@ -21,28 +21,6 @@
             return fromDateTime < toDateTime;
         };
 
-        //$scope.isValidDateRange = function (fromDate, fromTime, toDate, toTime) {
-        //    var fromDateTime = dateService.combineDateAndTime(fromDate, fromTime);
-        //    var toDateTime = dateService.combineDateAndTime(toDate, toTime);
-        //    return fromDateTime < toDateTime;
-        //};
-
-        //$scope.$watch('daterange.fromDate', function (newValue, oldValue) {
-        //    $scope.daterange.fromDateTime = dateService.combineDateAndTime($scope.daterange.fromDate, $scope.daterange.fromTime);
-        //});
-
-        //$scope.$watch('daterange.fromTime', function (newValue, oldValue) {
-        //    $scope.daterange.fromDateTime = dateService.combineDateAndTime($scope.daterange.fromDate, $scope.daterange.fromTime);
-        //});
-
-        //$scope.$watch('daterange.toDate', function (newValue, oldValue) {
-        //    $scope.daterange.toDateTime = dateService.combineDateAndTime($scope.daterange.toDate, $scope.daterange.toTime);
-        //});
-
-        //$scope.$watch('daterange.toTime', function (newValue, oldValue) {
-        //    $scope.daterange.toDateTime = dateService.combineDateAndTime($scope.daterange.toDate, $scope.daterange.toTime);
-        //});
-
         // From date control
         $scope.fromDateHasFocus = false;
         $scope.showFromDatePicker = false;
